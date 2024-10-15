0bb64f454e390fd6f11514ba01b8ebd5dbc0f658
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-10-15 19:15:38 -0400

    React course on Udemy

---
+++ Fundamentos/fund1/src/App.js
@@ -0,0 +1,15 @@
+// Components
+import FirstComponent from './components/firstComponents';
+// styles
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+    </div>
+  );
+}
+
+export default App;
--- project1/src/App.js
+++ project1/src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Testando o React!!</h1>
-      <p>Com creat-react-app(CRA)</p>
+      <h1>
+        Testando o <span>React!!</span>
+      </h1>
+      <p>
+        Com creat-react-app<span className="sp">(CRA)</span>
+      </p>
+      <button className="btn">🤪</button>
     </div>
   );
 }
