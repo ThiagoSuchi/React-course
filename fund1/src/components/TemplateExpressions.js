25cbf1ae8609f27bab3cb081dedb182ae993662e
THIAGO <thiagohenssuchi@gmail.com>
2024-10-17 14:50:24 -0400

    Resolvendo conflito

--- fund1/src/components/TemplateExpressions.js
+++ fund1/src/components/TemplateExpressions.js
@@ -4,13 +4,14 @@ const TemplateExpression = () => {
         idade: 21,
         cargo: 'Dev Front-end'
     }
+    const soma = 2+2
     
     return (
         <div>
             <h2>Fala rapaziadaa, sou o {nome}</h2>
             <p>sobre mim:</p>
             <p>- idade: {infoPessoa.idade}</p>
-            <p>- cargo: {infoPessoa.cargo}</p>
+            <p>- cargo: {infoPessoa.cargo}, {soma}</p>
         </div>
     )
 }
