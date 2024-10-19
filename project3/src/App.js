c8adb5b1e7ebb8143670e0a71f51d3c005a8a852
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-10-19 18:03:26 -0400

    Course task

---
+++ project3/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Realizando desafio!</h1>
+        <p>Dado pelo curso, afim de testar se aprendi a baixar e configurar todo o react</p>
+    </div>
+  );
+}
+
+export default App;
