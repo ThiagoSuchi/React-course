d693a4804e5fe1a34e1944704027b5f9cb4f4807
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-10-20 21:53:23 -0400

    adding images from the public folder and importing from the assets folder

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+// Imagem importada do assets
+import Riu from "./assets/StreetFt.jpg"
 
 function App() {
   return (
     <div className="App">
         <h1>Realizando desafio!</h1>
         <p>Dado pelo curso, afim de testar se aprendi a baixar e configurar todo o react</p>
+        
+        {/* Adicionando imagem do public */}
+        <div>
+          <img src="/Agnuz.jpg" alt="agnuz-img" />
+        </div>
+        {/* Imagem em assets */}
+        <div>
+          <img src={Riu} alt="StretFighter" />
+        </div>
     </div>
   );
 }
