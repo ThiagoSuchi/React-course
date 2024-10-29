22237ff4aa87424252b5b62683cc75a86e69949b
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-10-29 11:15:52 -0400

    re-rendering component - ternary condition, and button

--- project3/src/components/ConditionalRender.js
+++ project3/src/components/ConditionalRender.js
@@ -3,13 +3,30 @@ import { useState } from "react"
  export function ConditionalRender() {
     const [x] = useState(false)
 
+    const [nome, setName] = useState("Matheus")
+
     return (
     <div>
         <h1>Isso será exibido?</h1>
+
         {/* Esse é a condicional mais simples do React! */}
         {x && <p>Se x for true, sim!</p>}
+        
         {/* Agora o x se tornou false, devido ao ! */}
         {!x && <p>Agora o x é falso!</p>}
+
+        {/* Condição ternário */}
+        {nome === "João" ? (
+            <div>
+                <p>Seja bem-vindo João</p>
+            </div>
+        ) : (
+            <div>
+                <p>Ops! parece que você não é o João, acesso negado!</p>
+            </div>
+        )}
+        {/* Re-rederizando componente com button */}
+        <button onClick={() => setName('João')}>Clique aqui!</button>
     </div>
   )
 }
