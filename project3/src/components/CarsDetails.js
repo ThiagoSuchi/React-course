07dc2bd398c6583454c24e6124cfe6f29076356d
THIAGO <thiagohenssuchi@gmail.com>
2024-11-14 16:39:47 -0400

    Destructure - Fragments - List rendering in components

--- project3/src/components/CarsDetails.js
+++ project3/src/components/CarsDetails.js
@@ -1,6 +1,5 @@
 // Destructuring (ou desestruturação) em props é uma técnica em JavaScript, especialmente no contexto do React, que permite extrair valores de objetos e arrays de forma mais concisa e legível. 
 
-
 export const CarsDetails = ({brand, km, color = "Preto", newCar}) => {
 // Ao invés de acessar as props através do objeto props, você pode desestruturar as propriedades desejadas diretamente nos parâmetros da função do componente.
   return (
