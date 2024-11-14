1af9f99d4bfd98b3ade930dbb11f93d87d518a34
THIAGO <thiagohenssuchi@gmail.com>
2024-11-14 16:53:31 -0400

    Conditional - Ternary

--- project3/src/components/CarsDetails.js
+++ project3/src/components/CarsDetails.js
@@ -1,7 +1,11 @@
 // Destructuring (ou desestruturação) em props é uma técnica em JavaScript, especialmente no contexto do React, que permite extrair valores de objetos e arrays de forma mais concisa e legível. 
 
+import { useState } from "react"
+
 export const CarsDetails = ({brand, km, color = "Preto", newCar}) => {
 // Ao invés de acessar as props através do objeto props, você pode desestruturar as propriedades desejadas diretamente nos parâmetros da função do componente.
+  const [kmCarro] =  useState(km) 
+
   return (
     <div>
         <h1>Detalhes do carro</h1>
@@ -10,8 +14,11 @@ export const CarsDetails = ({brand, km, color = "Preto", newCar}) => {
             <li>KM: {km}</li>
             <li>Color: {color}</li>
         </ul>
-        {newCar && <p>Este carro é novo, 0km!</p>}
-        {!newCar && <p>Este carro é usado!</p>}
+        {kmCarro > 0 ? (
+            <p>Descrição: O carro é usado, {km}km rodados</p>
+          ) : (
+            <p>Descrição: O carro é novo, 0km!</p>
+          )}
     </div>
   )
 }
