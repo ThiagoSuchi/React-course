6e45813b5d435ac127d8c7bc984d423338e9554f
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-11-16 12:27:12 -0400

    Executing functions as a props

---
+++ project3/src/components/ExecutarFunction.js
@@ -0,0 +1,10 @@
+const ExecutarFunction = ({ myMessage }) => {
+  return (
+    <div>
+        <button onClick={myMessage}>Clique aqui!</button>
+        <p>Olhe no devTools</p>
+    </div>
+  )
+}
+
+export default ExecutarFunction
