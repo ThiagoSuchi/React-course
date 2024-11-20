9a1e0b47b71aea9649ae9ec9329d420e11f5e079
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-11-20 19:07:39 -0400

    Inline CSS dynamic

--- projectCSS/src/App.js
+++ projectCSS/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
 import MyComponent from './components/MyComponent';
+import { useState } from "react";
 
 function App() {
+  const n = 12;
+  const [name] = useState("Isabelle")
+
   return (
     <div className="App">
       {/* CSS global */}
@@ -10,7 +14,18 @@ function App() {
       {/* CSS do componente */}
       <MyComponent/>
       {/* Inline CSS */}
-      <p style={{color: "blue", padding: "25px", border: "2px solid red"}}>Este elemento foi estilizado de forma inline</p>
+      <p style={{color: "blue", padding: "25px", border: "2px solid red"}}>
+        Este elemento foi estilizado de forma inline
+      </p>
+      {/* Inline css dinâmico */}
+      <h2 style={n > 10 ? {color: "green"} : {color: "grey"}}>Css dinâmico</h2>
+      <h2 style={n < 10 ? {color: "green"} : {color: "grey"}}>Css dinâmico</h2>
+      <p style={
+        name === "Isabelle" 
+          ? {color: "white", backgroundColor: "grey"}
+          : null
+        }>
+     {name}</p>
     </div>
   );
 }
