f452a4531161409d53cae9459955a71e3cd86ee0
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-11-20 11:53:48 -0400

    Inline CSS

--- projectCSS/src/App.js
+++ projectCSS/src/App.js
@@ -4,9 +4,13 @@ import MyComponent from './components/MyComponent';
 function App() {
   return (
     <div className="App">
+      {/* CSS global */}
       <h1>React com CSS</h1>
       <p>Esse é o parágrafo do pai!</p>
+      {/* CSS do componente */}
       <MyComponent/>
+      {/* Inline CSS */}
+      <p style={{color: "blue", padding: "25px", border: "2px solid red"}}>Este elemento foi estilizado de forma inline</p>
     </div>
   );
 }
