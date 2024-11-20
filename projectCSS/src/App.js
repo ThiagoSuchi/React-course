103097ed630441342c7633d1b340dbe453725a37
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-11-20 11:33:24 -0400

    Rendering css global and component css

---
+++ projectCSS/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import MyComponent from './components/MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React com CSS</h1>
+      <p>Esse é o parágrafo do pai!</p>
+      <MyComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ projectCSS/src/components/MyComponent.js
@@ -0,0 +1,12 @@
+import './MyComponent.css'
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>Meu componente</h1>
+        <p className='pcomp'>Esse é meu parágrafo do componente!</p>
+    </div>
+  )
+}
+
+export default MyComponent
