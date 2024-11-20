3d3a1243b667e7745bfc8b7a4f377fcba401f153
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-11-20 19:42:02 -0400

    Css MOdule - Scoped

--- projectCSS/src/components/Title.js
+++ projectCSS/src/components/Title.js
@@ -3,6 +3,7 @@ import styles from "./Title.module.css"
 const Title = () => {
   return (
     <div>
+        {/* Scoped - limitar a aplicação de estilos, lógica ou estado a um componente específico. */}
        <h1 className={styles.my_title}>My title</h1>
     </div>
   )
