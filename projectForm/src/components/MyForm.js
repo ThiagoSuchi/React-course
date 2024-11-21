5aa635a80ecfbb2ad40791f20e450471265cd376
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-11-21 15:25:58 -0400

    Input label management - hook  useState

--- projectForm/src/components/MyForm.js
+++ projectForm/src/components/MyForm.js
@@ -1,23 +1,40 @@
-import Style from "./MyForm.css"
+import "./MyForm.css";
+import { useState } from "react";
 
 const MyForm = () => {
+  // 3 - gerenciamento de dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  };
+
+console.log(name);
+
+
   return (
     <div>
-      {/* Criação de forms */}
+      {/* 1 - Criação de forms */}
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite seu nome" />
+          <input
+            type="text"
+            name="name"
+            placeholder="Digite seu nome"
+            onChange={handleName}
+          />
         </div>
-        {/* Label envolvendo input */}
+        {/* 2 - Label envolvendo input */}
         <label>
           <span>E-mail</span>
           <input type="email" name="email" placeholder="Digite seu e-mail" />
         </label>
-        <input type="submit" value="Enviar"/>
+        <input type="submit" value="Enviar" />
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default MyForm
+export default MyForm;
