cc83774f84a6867beaddbf74782b04893f5fe2dc
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-11-21 15:15:00 -0400

    Labels involving inputs

--- projectForm/src/components/MyForm.js
+++ projectForm/src/components/MyForm.js
@@ -9,6 +9,11 @@ const MyForm = () => {
           <label htmlFor="name">Nome:</label>
           <input type="text" name="name" placeholder="Digite seu nome" />
         </div>
+        {/* Label envolvendo input */}
+        <label>
+          <span>E-mail</span>
+          <input type="email" name="email" placeholder="Digite seu e-mail" />
+        </label>
         <input type="submit" value="Enviar"/>
       </form>
     </div>
