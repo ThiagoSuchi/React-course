f7c17c41324fb0c2d0095b96b2277711ad6cef0b
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-11-21 15:08:56 -0400

    Creatingforms

--- projectForm/src/App.js
+++ projectForm/src/App.js
@@ -1,9 +1,13 @@
+// Component
+import MyForm from "./components/MyForm";
+// Styles
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      
+      <h2>Forms</h2>
+      <MyForm/>
     </div>
   );
 }
