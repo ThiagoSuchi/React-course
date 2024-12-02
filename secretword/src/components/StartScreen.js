31ab8e44b39003c00b9d358dcba06467529dc77e
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-02 10:20:18 -0400

    Projeto Secret Word, preparando ambiente - Estilização básica feita.

---
+++ secretword/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import "./StartScreen.css";
+
+const StartScreen = () => {
+  return (
+    <div className="start">
+      <h1>Secret Word</h1>
+      <p>Bora jogar? ⬇️Clique no botão abaixo⬇️</p>
+      <button>jogar</button>
+    </div>
+  );
+};
+
+export default StartScreen;
