80295929f4ba3a295b791b6dc9e688a53786e8fb
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-02 09:53:45 -0400

    Projeto Secret Word, preparando ambiente...

---
+++ secretword/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+    carro: ['Motor', 'Porta', 'Capô', 'Pneu', 'Antena', 'Volante', 'Retrovisor', 'Freio de mão', 'Marcha', 'Acelerador', 'Freio', 'Embreagem'],
+    fruta: ['Banana', 'Maçã', 'Pêra', 'Mamão', 'Laranja', 'Uva', 'Melancia', 'Abacate', 'Melão', 'Morango', 'Kiwi'],
+    corpo: ['Braço', 'Perna', 'Cérebro', 'pescoço', 'Olhos', 'Nariz', 'Orelhas', 'Mão', 'Boca', 'Coração'],
+    computador: ['Mouse', 'Teclado', 'Monitor', 'Gabinete', 'Processador', 'Placa Mãe', 'Memória RAM', 'Fonte', 'Placa de Vídeo'],
+    programacao: ['Linguagem', 'Framework', 'JavaScript', 'React', 'Java', 'TypeScript', 'Flotter'],
+    alimento: ['Arroz', 'Feijão', 'Carne', 'Leite', 'Ovo', 'Macarrão', 'Strogonofe']
+}
