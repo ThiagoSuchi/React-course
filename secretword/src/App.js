889c56dc2606aebef7cdafc36549d139f0b1b8bc
Thiago Hens Suchi <thiagohenssuchi@gmail.com>
2024-12-06 10:54:12 -0400

    Adiconando evento de inserção de letra

--- secretword/src/App.js
+++ secretword/src/App.js
@@ -66,8 +66,8 @@ function App() {
   };
 
   // Process the letter input
-  const verifyLetter = () => {
-    setGameStage(stages[2].name);
+  const verifyLetter = (letter) => {
+    console.log(letter);
   };
 
   // Game-over, restart process
