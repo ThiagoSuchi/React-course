598efb9d3360f1468868971b600fafc09df8feff
ThiagoSuchi <thiagohenssuchi@gmail.com>
2025-10-13 21:34:34 -0400

    Loading state

--- REQ-HTTP/httpreact/src/App.jsx
+++ REQ-HTTP/httpreact/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   const [products, setProducts] = useState([]);
 
   // 4 - custom hook
-  const { data: items } = useFetch(url);
+  const { data: items, httpConfig, loading } = useFetch(url);
 
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
@@ -32,22 +32,25 @@ function App() {
     e.preventDefault();
 
     const product = {
-      name, 
+      name,
       price
     };
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(product),
-    });
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify(product),
+    // });
 
-    // 3 - Carregamento dinâmico
-    const addedProduct = await res.json()
+    // // 3 - Carregamento dinâmico
+    // const addedProduct = await res.json()
 
-    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    // 5 - Refatorando POST
+    httpConfig(product, "POST");
 
     setName("");
     setPrice("");
@@ -56,13 +59,17 @@ function App() {
   return (
     <>
       <h1>Lista de produtos</h1>
-      <ul>
-        {items && items.map((product) => (
-          <li key={product.id}>
-            {product.name} - R$ {product.price}
-          </li>
-        ))}
-      </ul>
+      {/* 6 - Loading */}
+      {loading && <p>Carregando dados...</p>}
+      {!loading && (
+        <ul>
+          {items && items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$ {product.price}
+            </li>
+          ))}
+        </ul>
+      )}
       <div className='add-product'>
         <form onSubmit={handlesubmit}>
           <label>
