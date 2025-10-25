f1d350ef9a4e58ba12f65444bfe0574138e015ce
ThiagoSuchi <thiagohenssuchi@gmail.com>
2025-10-25 11:57:54 -0400

    Passing user to context, creating links to authentication user

--- Safira/src/App.jsx
+++ Safira/src/App.jsx
@@ -6,21 +6,43 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 
+// hooks
+import { useState, useEffect, use } from 'react';
+import { useAuthentication } from './hooks/useAuthentication';
+
+// context
+import { AuthProvider } from './context/AuthContext';
+import { onAuthStateChanged } from 'firebase/auth';
+
 // pages
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
-
-// context
-import { AuthProvider } from './context/AuthContext';
+import CreatePost from './pages/CreatePost/CreatePost';
+import Dashboard from './pages/Dashboard/Dashboard';
 
 function App() {
+  const [user, setUser] = useState(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    })
+
+  }, [auth]);
+
+  if (loadingUser) {
+    return <p>Carregando...</p>
+  }
 
   return (
     <>
       <div className="App">
-        <AuthProvider>
+        <AuthProvider value={{ user }}>
           <BrowserRouter>
             <Navbar />
             <div className="container">
@@ -29,6 +51,8 @@ function App() {
                 <Route path="/about" element={<About />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/posts/create" element={<CreatePost />} />
+                <Route path="/dashboard" element={<Dashboard />} />
               </Routes>
             </div>
             <Footer />
--- Safira/src/components/Navbar/Navbar.jsx
+++ Safira/src/components/Navbar/Navbar.jsx
@@ -1,32 +1,52 @@
 import { NavLink } from 'react-router-dom';
 import safiraLogo from '../../assets/icon/safiraLogo.svg';
 
+import { useAuthentication } from '../../hooks/useAuthentication';
+
+import { useAuthValue } from '../../context/AuthContext';
+
 // CSS
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-        <NavLink to="./" className={styles.brand}>
-            <img src={safiraLogo} alt="Safira Logo" />
-            Safira <span>Blog</span>
-        </NavLink>
-        <ul className={styles.links_list}>
-            <li>
-                <NavLink to="/" className={({isActive}) => (isActive ? styles.active : "")}>Início</NavLink>
-            </li>
-            <li>
-                <NavLink to="/about" className={({isActive}) => (isActive ? styles.active : "")}>Sobre</NavLink>
-            </li>
-            <li>
-                <NavLink to="/login" className={({isActive}) => (isActive ? styles.active : "")}>Entrar</NavLink>
-            </li>
-            <li>
-                <NavLink to="/register" className={({isActive}) => (isActive ? styles.active : "")}>Cadastrar-se</NavLink>
-            </li>
-        </ul>
-    </nav>
-  )
+    const { user } = useAuthValue();
+
+    return (
+        <nav className={styles.navbar}>
+            <NavLink to="./" className={styles.brand}>
+                <img src={safiraLogo} alt="Safira Logo" />
+                Safira <span>Blog</span>
+            </NavLink>
+            <ul className={styles.links_list}>
+                <li>
+                    <NavLink to="/" className={({ isActive }) => (isActive ? styles.active : "")}>Início</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about" className={({ isActive }) => (isActive ? styles.active : "")}>Sobre</NavLink>
+                </li>
+                {!user && (
+                    <>
+                        <li>
+                            <NavLink to="/login" className={({ isActive }) => (isActive ? styles.active : "")}>Entrar</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/register" className={({ isActive }) => (isActive ? styles.active : "")}>Cadastrar-se</NavLink>
+                        </li>
+                    </>
+                )}
+                {user && (
+                    <>
+                        <li>
+                            <NavLink to="/posts/create" className={({ isActive }) => (isActive ? styles.active : "")}>Novo Post</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/dashboard" className={({ isActive }) => (isActive ? styles.active : "")}>Dashboard</NavLink>
+                        </li>
+                    </>
+                )}
+            </ul>
+        </nav>
+    )
 }
 
 export default Navbar
